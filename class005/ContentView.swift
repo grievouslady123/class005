9d5ba0991d98ee7ce49ed66aa6df9b94006ea19e
grievouslady123 <li980109@gmail.com>
2022-10-25 21:23:33 +0800

    Initial Commit

---
+++ class005/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  class005
+//
+//  Created by 暨大附中１ on 2022/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ class005/class005App.swift
@@ -0,0 +1,17 @@
+//
+//  class005App.swift
+//  class005
+//
+//  Created by 暨大附中１ on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct class005App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
